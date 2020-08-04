eb4450ba8ee1af62f6e7da8e7afa460a3672109d
Ardhana <mardhanawn@gmail.com>
2020-08-04 09:49:48 +0700

    initial commit

---
+++ src/components/Checkout.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        <h1>Checkout</h1>
+        <table class="table table-hover" v-if="cart.length">
+            <caption class="text-right h3">
+                <b>Total : </b>
+                <price class="d-block text-success font-weight-light"
+                    :value="Number(cartTotal)"></price>
+            </caption>
+            <thead>
+                <tr>
+                    <th scope="col"></th>
+                    <th scope="col">Item</th>
+                    <th scope="col" class="text-center">Qty</th>
+                    <th scope="col" class="text-right">Price</th>
+                    <th scope="col" class="text-right">Sub-total</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(item, index) in cart" :key="item.product.id">
+                    <td class="text-center">
+                        <div class="btn-group">
+                            <button @click="$emit('add', item.product)" class="btn btn-info">+</button>
+                            <button @click="$emit('delete', index)" class="btn btn-outline-info">-</button>
+                        </div>
+                    </td>
+                    <th scope="row">{{ item.product.price }}</th>
+                    <th scope="text-center">{{ item.qty }}</th>
+                    <th scope="text-right">{{ Number(item.product.price) }}</th>
+                    <th scope="text-right">{{ item.product.price * item.qty }}</th>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+import Price from './Price.vue'
+
+export default {
+    name: 'checkout',
+    props: ['cart', 'cartTotal'],
+    components: {
+        Price
+    }
+}
+</script>
